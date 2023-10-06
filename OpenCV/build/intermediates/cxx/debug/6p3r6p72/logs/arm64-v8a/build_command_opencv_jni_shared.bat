3e3e97e4bb8adf38af794140848583a703f3bbc7
rskl1999 <127722354+rskl1999@users.noreply.github.com>
2023-10-06 22:20:19 +0800

    new

---
+++ OpenCV/build/intermediates/cxx/debug/6p3r6p72/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Kiel\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\ForHuefirst\\OpenCV\\.cxx\\Debug\\6p3r6p72\\arm64-v8a" ^
+  opencv_jni_shared
